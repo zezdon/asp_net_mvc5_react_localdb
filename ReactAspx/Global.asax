db53afff9dd2b7775bacaa9de1b19dcda126ee74
Mats G <ed54res@hotmail.com>
2019-07-31 15:49:50 +0200

    Add project files.

---
+++ ReactAspx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactAspx.MvcApplication" Language="C#" %>
---
+++ ReactAspx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactAspx.Startup))]
+namespace ReactAspx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
